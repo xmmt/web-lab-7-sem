ede26860503c89799f114d970917802d126b9b79
Maksim Mashkov <mkmax472@gmail.com>
2019-12-04 05:46:59 +0300

    fetch and show weather

--- weather-1/src/index.js
+++ weather-1/src/index.js
@@ -1,10 +1,36 @@
-const template = `
-    Welcome to dust, {user}!
+const APIKey = "7b6d16b9aad94f104b9310d42ef9ebf0";
+const weatherTemplate = `
+<p>City, Region: {name}, {sys.country}</p>
+<p>Weather:{#weather}{description}, {/weather}
+{#main}
+temperature: {temp} K, 
+humidity: {humidity}%, 
+pressure: {pressure} hPa, 
+{/main}
+wind speed: {wind.speed} meters per hour.
+</p>
 `;
-const compiled = dust.compile(template, "tmp1");
-dust.loadSource(compiled);
-const weatherDiv = document.getElementById("weather");
+const errorTemplate = `
+<p>Error: {message}</p>
+`;
+const weatherCompiled = dust.compile(weatherTemplate, "weatherTemplate");
+const errorCompiled = dust.compile(errorTemplate, "errorTemplate");
+dust.loadSource(weatherCompiled);
+dust.loadSource(errorCompiled);
+const weatherDiv = document.getElementById("weather-div");
+const cityInput = document.getElementById("city-input");
 
-dust.render("tmp1", { user: "UserName" }, function(err, out) {
-  weatherDiv.innerHTML = out;
-});
+async function onsubmit(e) {
+  const inputValue = document.getElementById("city-input").value;
+  e.preventDefault();
+  if (inputValue === "") {
+    weatherDiv.innerHTML = "<p>Empty city name</p>";
+    return;
+  }
+  const weather = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&APPID=${APIKey}`);
+  const weatherJson = await weather.json();
+  await dust.render(weather.ok ? "weatherTemplate" : "errorTemplate", weatherJson, function(err, out) {
+    weatherDiv.innerHTML = out;
+  });
+}
+document.getElementById("city-form").addEventListener("submit", onsubmit);
