48b8aacca97de79834eb98961f7bb11c252ed35b
Maksim Mashkov <mkmax472@gmail.com>
2019-12-04 04:33:43 +0300

    add simple template

--- weather-1/src/index.js
+++ weather-1/src/index.js
@@ -1,2 +1,10 @@
-// eslint-disable-next-line no-alert
-alert('hello');
+const template = `
+    Welcome to dust, {user}!
+`;
+const compiled = dust.compile(template, "tmp1");
+dust.loadSource(compiled);
+const weatherDiv = document.getElementById("weather");
+
+dust.render("tmp1", { user: "UserName" }, function(err, out) {
+  weatherDiv.innerHTML = out;
+});
