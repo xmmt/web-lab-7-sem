8e55d01aad041233952eaebf166959b9ce7a3af3
Maksim Mashkov <mkmax472@gmail.com>
2020-02-07 19:32:40 +0300

    usePosition hook for geolocation

---
+++ weather-2/src/usePosition.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+
+const usePosition = () => {
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState(null);
+
+  const onChange = ({ coords }) => {
+    const { latitude, longitude } = coords;
+    // Здесь мы могли бы сохранить весь объект position, но для
+    // ясности давайте явно перечислим, какие свойства нас интересуют.
+    setPosition({ latitude, longitude });
+  };
+
+  const onError = err => {
+    setError(err.message);
+  };
+
+  useEffect(() => {
+    const geo = navigator.geolocation;
+
+    if (!geo) {
+      setError("Геолокация не поддерживается браузером");
+      return;
+    }
+
+    // Подписываемся на изменение геопозиции браузера.
+    const watcher = geo.watchPosition(onChange, onError);
+
+    // В случае, если компонент будет удаляться с экрана
+    // производим отписку от слежки, чтобы не засорять память.
+    return () => geo.clearWatch(watcher);
+  }, []);
+
+  return { ...position, error };
+};
+
+export default usePosition;
