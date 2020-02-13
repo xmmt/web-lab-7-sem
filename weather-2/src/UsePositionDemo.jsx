aa45ab8242cf99bff3a1aa7e7f6a107e998ea5a8
Maksim Mashkov <mkmax472@gmail.com>
2020-02-13 22:20:16 +0300

    add redux

--- weather-2/src/UsePositionDemo.jsx
+++ weather-2/src/UsePositionDemo.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
-// Импортируем наш хук здесь.
 import usePosition from './usePosition';
 
 const UsePositionDemo = () => {
-  // Получаем позицию браузера (или ошибку) здесь.
   const { latitude, longitude, error, getLocation } = usePosition();
 
-  // Выводим координаты на экран одной строкой (красота пока не нужна).
   return (
     <>
       latitude: {latitude}, longitude: {longitude}, error: {error}
-      <button type="button" onClick={getLocation}>
+      <button className="btn" type="button" onClick={getLocation}>
         Узнать местоположение
       </button>
     </>
--- weather-2/src/usePosition.js
+++ weather-2/src/usePosition.js
@@ -6,8 +6,6 @@ const usePosition = () => {
 
   const onChange = ({ coords }) => {
     const { latitude, longitude } = coords;
-    // Здесь мы могли бы сохранить весь объект position, но для
-    // ясности давайте явно перечислим, какие свойства нас интересуют.
     setPosition({ latitude, longitude });
   };
 
@@ -32,11 +30,8 @@ const usePosition = () => {
       return;
     }
 
-    // Подписываемся на изменение геопозиции браузера.
     const watcher = geo.watchPosition(onChange, onError);
 
-    // В случае, если компонент будет удаляться с экрана
-    // производим отписку от слежки, чтобы не засорять память.
     return () => geo.clearWatch(watcher);
   }, []);
 
